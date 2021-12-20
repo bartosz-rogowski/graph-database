6ae7af33b75df615740e45b16238672c9469b9d7
bartosz-rogowski <bartoz3rogowski@gmail.com>
2021-12-20 14:23:32 +0100

    final touch

--- mysite/__init__.py
+++ mysite/__init__.py
@@ -4,7 +4,6 @@ from py2neo import Graph, Node, Relationship
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ce50bd8fbfbeb7391c109aca88e6ff16'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = Graph("neo4j+s://8d5ae205.databases.neo4j.io",
 	auth = ("neo4j", "URtjTjSqnpB2FoztE8tYDhU3ujIGy8WAvoAMZfrU9eE"))
 
